908b044f4d0fabf95ec588bb47b86b6de0378984
Paul Kinlan <paul.kinlan@gmail.com>
2015-07-08 13:16:29 +0100

    Lots of changes to material

--- _material/docs/_assets/main.js
+++ _material/docs/_assets/main.js
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*global MaterialCustomizer:true,Prism:true,ga:true*/
+
 // Navbar scroll buttons
 (function() {
   'use strict';
@@ -29,4 +47,20 @@
   rightScroll.addEventListener('tap', scrollMenuBar.bind(null, delta));
   leftScroll.addEventListener('click', scrollMenuBar.bind(null, -delta));
   leftScroll.addEventListener('tap', scrollMenuBar.bind(null, -delta));
+})();
+
+(function() {
+  'use strict';
+
+  var downloadButton = document.querySelector('.download-button-container');
+  if (downloadButton) {
+    downloadButton.addEventListener('click', function() {
+      ga('send', {
+        hitType: 'event',
+        eventCategory: 'click',
+        eventAction: 'mdl-download'
+      });
+    });
+  }
+
 })();
