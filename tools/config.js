0e17b3f28938be3eac0b1a06373f41db0f7dc8fb
Paul Kinlan <paul.kinlan@gmail.com>
2015-08-04 10:08:45 +0100

    Testing out bigrig

---
+++ tools/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "tasks": {
+    "Home Page (Cable, Desktop)": {
+      "url": "https://paul.kinlan.me/",
+      "labels": "homepage"
+    },
+
+    "Home Page (3G, Motorola G)": {
+      "url": "https://paul.kinlan.me/",
+      "labels": "homepage-3g",
+      "location": "Dulles_MotoG:Motorola G - Chrome Dev",
+      "connectivity": "3G"
+    },
+  }
+}
