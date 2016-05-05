5e294248ffa3f4746c8796de3a36fcb92cd1a300
Paul Kinlan <paul.kinlan@gmail.com>
2016-05-05 14:35:12 +0100

    Adding a service worker

--- static/sw.js
+++ static/sw.js
@@ -1,27 +1,37 @@
-// v1.0 
+const version = "1.1";
+const fetchAndCache = request => fetch(request).then(response => {
+  return caches.open(version).then(cache => {
+    const clone = response.clone();
+    cache.put(request, response);
+    return clone;
+  })
+});
 
-self.addEventListener("fetch", function(event) {
-  var requestURL = new URL(event.request.url);
+// Promise.race is no good to us because it rejects if
+// a promise rejects before fulfilling. Let's make a proper
+// race function:
+function promiseAny(promises) {
+  return new Promise((resolve, reject) => {
+    // make sure promises are all promises
+    promises = promises.map(p => Promise.resolve(p));
+    // resolve this promise as soon as one resolves
+    promises.forEach(p => p.then(resolve));
+    // reject if all promises reject
+    promises.reduce((a, b) => a.catch(() => b))
+      .catch(() => reject(Error("All failed")));
+  });
+};
+
+self.addEventListener('fetch', function(event) {
+  var request = event.request;
   
-  if (requestURL.origin == location.origin) {
-    var dm = 0;
-    var navType = "unknown";
-    
-    if(navigator.connection && navigator.connection.downlinkMax !== false) {
-      dm = navigator.connection.downlinkMax;
-    }
-    
-    if(navigator.connection && navigator.connection.type !== false) {
-      navType = navigator.connection.type;
-    }
-    
-    event.respondWith(
-      fetch(event.request.url, {
-        headers: { 
-          'MD': dm,
-          'Network-Type': navType
-        }
-      })
-    );
-  }
+  if(request.url.indexOf("https://www.google-analytics.com/collect") === 0 ||
+     request.url.indexOf("http://www.google-analytics.com/collect") === 0 ) return;
+  
+  event.respondWith(
+    promiseAny([
+      caches.match(request),
+      fetchAndCache(request)
+    ])
+  );
 });
