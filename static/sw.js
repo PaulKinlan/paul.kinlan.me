aecc77b71d1d26ae19f01b4b9dcdb6fcfa85d136
Paul Kinlan <paul.kinlan@gmail.com>
2016-05-07 20:55:26 +0100

    Updating service worker

--- static/sw.js
+++ static/sw.js
@@ -1,26 +1,4 @@
-const version = "1.1";
-const fetchAndCache = request => fetch(request).then(response => {
-  return caches.open(version).then(cache => {
-    const clone = response.clone();
-    cache.put(request, response);
-    return clone;
-  })
-});
-
-// Promise.race is no good to us because it rejects if
-// a promise rejects before fulfilling. Let's make a proper
-// race function:
-function promiseAny(promises) {
-  return new Promise((resolve, reject) => {
-    // make sure promises are all promises
-    promises = promises.map(p => Promise.resolve(p));
-    // resolve this promise as soon as one resolves
-    promises.forEach(p => p.then(resolve));
-    // reject if all promises reject
-    promises.reduce((a, b) => a.catch(() => b))
-      .catch(() => reject(Error("All failed")));
-  });
-};
+const version = "1.2";
 
 self.addEventListener('fetch', function(event) {
   var request = event.request;
@@ -29,13 +7,17 @@ self.addEventListener('fetch', function(event) {
   if(url.origin !== location.origin) return;
   
   event.respondWith(
-    promiseAny([
-      caches.match(request),
-      fetchAndCache(request)
-    ])
+   caches.open(version).then(cache => {
+      return cache.match(request).then(response => {
+        var fetchPromise = fetch(request).then(networkResponse => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        })
+        return response || fetchPromise;
+      })
+    })
   );
 });
-
 
 self.addEventListener("push", e => {
    let options = { 
