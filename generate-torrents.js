121f709deaecfafb6bcb4924c446a8103954bd80
Paul Kinlan <paul.kinlan@gmail.com>
2016-07-15 12:39:04 +0100

    Cleaning up torrents information, adding videos.

--- generate-torrents.js
+++ generate-torrents.js
@@ -3,49 +3,30 @@
 var createTorrent = require('create-torrent')
 var fs = require('fs')
 
-var podcastCandidates = fs.readdirSync('static/podcasts/');
-var podcasts = [];
+const processFiles = function(folder) {
+    var candidates = fs.readdirSync(`static/${folder}/`);
+    var files = [];
 
-for(var i = 0; i < podcastCandidates.length; i++) {
-    
-    let podcast = podcastCandidates[i];
+    for(var i = 0; i < candidates.length; i++) {
+        
+        let candidate = candidates[i];
 
-    if(podcast.endsWith('.mp3') || podcast.endsWith("webm")) {
-      podcasts.push(podcast);
+        if(candidate.endsWith('.mp4') || candidate.endsWith('.mp3') || candidate.endsWith("webm")) {
+            files.push(candidate);
+        }
     }
+
+    files.forEach(file => {
+        createTorrent(`static/${folder}/${file}`, {
+            urlList: [`https://paul.kinlan.me/${folder}/${file}`]
+            }, function (err, torrent) {
+                if (!err) {
+                    // `torrent` is a Buffer with the contents of the new .torrent file 
+                    fs.writeFile(`static/${folder}/${file}.torrent`, torrent)
+                }
+        })
+    });
 }
 
-podcasts.forEach(podcast => {
-    createTorrent(`static/podcasts/${podcast}`, {
-        urlList: [`https://paul.kinlan.me/podcasts/${podcast}`]
-        }, function (err, torrent) {
-            if (!err) {
-                // `torrent` is a Buffer with the contents of the new .torrent file 
-                fs.writeFile('static/podcasts/' + podcast + '.torrent', torrent)
-            }
-    })
-});
-
-
-var videoCandidates = fs.readdirSync('static/videos/');
-var videos = [];
-
-for(var i = 0; i < videoCandidates.length; i++) {
-    
-    let video = videoCandidates[i];
-
-    if(video.endsWith('.mp4') || video.endsWith("webm")) {
-      videos.push(video);
-    }
-}
-
-videos.forEach(video => {
-    createTorrent(`static/videos/${video}`, {
-        urlList: [`https://paul.kinlan.me/videos/${video}`]
-        }, function (err, torrent) {
-            if (!err) {
-                // `torrent` is a Buffer with the contents of the new .torrent file 
-                fs.writeFile('static/videos/' + video + '.torrent', torrent)
-            }
-    })
-})
+processFiles('podcasts');
+processFiles('videos');
