d3097027f4136946ae7a47e098501697ce70641e
Paul Kinlan <paul.kinlan@gmail.com>
2017-01-08 20:33:43 +0000

    Making Hugo build work.

--- install-hugo.sh
+++ install-hugo.sh
@@ -4,8 +4,8 @@ set -x
 set -e
 
 # Install Hugo if not already cached or upgrade an old version.
-if [ ! -e /tmp/bin/hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
-  wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz /tmp
-  tar xvzf /tmp/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
-  cp /tmp/hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 ./hugo
+if [ ! -e .hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
+  wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz -P .hugo/
+  tar xvzf .hugo/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz -C .hugo/
+  cp .hugo/hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 ./hugo
 fi
