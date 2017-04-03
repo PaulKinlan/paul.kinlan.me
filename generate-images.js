1d83f39ce896eaee692bc62420a2649ae01ceb55
Paul Kinlan <paul.kinlan@gmail.com>
2017-04-04 00:03:17 +0100

    Adding in new section and generation tools.

---
+++ generate-images.js
@@ -0,0 +1,53 @@
+"use strict";
+
+var fs = require('fs');
+var exif = require('exif').ExifImage;
+var yaml = require('js-yaml');
+const outputPath = 'content/life/';
+
+const processFiles = function(folder) {
+  const inputPath = `static/${folder}/`;
+  var candidates = fs.readdirSync(inputPath);
+  var files = [];
+
+  for(var i = 0; i < candidates.length; i++) {
+    let candidate = candidates[i];
+
+    if(candidate.endsWith('.jpg') || candidate.endsWith('.jpeg') || candidate.endsWith(".png")) {
+      files.push(candidate);
+    }
+  }
+
+  files.forEach(file => {
+    let mdPath = `${outputPath}/${file}.md`;
+    fs.stat(mdPath, function(statError, stat) {
+      if(true || statError) {
+
+        try {
+          new exif({image: `${inputPath}${file}`}, function(error, data) {
+            const imageDate = data.image.ModifyDate.split(' ')[0].replace(/:/g, '-');
+            const imageName = file.replace(/(\.jpg$|\.jpeg$)/,'');
+            
+            data.image.path = `/${folder}/${file}`;
+            data.image.name = imageName;
+            const imageYaml = yaml.safeDump(data, {});
+            console.log(data)
+            const template = `---
+date: ${imageDate}
+slug: ${imageName}
+title: ${imageName}
+${imageYaml}
+---`;
+        fs.writeFile(mdPath, template);
+          })
+
+        } catch (error) {
+          console.log(`Error parsing image: ${error.message}`);
+        }
+      }
+    });
+
+  });
+}
+
+processFiles('life');
