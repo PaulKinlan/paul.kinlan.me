f46ffa0d50b74b8d685768815b813a9b91ba4045
Paul Kinlan <paul.kinlan@gmail.com>
2017-04-04 11:18:28 +0100

    Adding Thumbnail support to build

--- generate-images.js
+++ generate-images.js
@@ -3,21 +3,24 @@
 var fs = require('fs');
 var exif = require('exif').ExifImage;
 var yaml = require('js-yaml');
-const outputPath = 'content/life/';
+var thumb = require('node-thumbnail').thumb;
 
+const outputPath = 'content/life/';
 
 const generateMarkdown = (inputPath, file, outputPath)  => {
   fs.stat(outputPath, function(statError, stat) {
     if(statError) {
 
       try {
+        console.log(`exif ${inputPath}...${file}`)
         new exif({image: `${inputPath}${file}`}, function(error, data) {
           try {
             const imageDate = data.exif.CreateDate.split(' ')[0].replace(/:/g, '-');
             const imageName = file.replace(/(\.jpg$|\.jpeg$|\.png$|\.gif$)/,'');
-            
-            data.image.path = `/${folder.replace('static')}${file}`;
+            const newInptutPath = inputPath.replace('static','');
+            data.image.path = `${newInptutPath}${file}`;
             data.image.name = imageName;
+            data.image.thumb_path = `${newInptutPath}thumb_${file}`;
             const imageYaml = yaml.safeDump(data, {});
             const template = `---
 date: ${imageDate}
@@ -27,6 +30,7 @@ ${imageYaml}
 ---`;
             fs.writeFile(outputPath, template);
           } catch(err) {
+            
             console.log(`Error parsing image after exif ${file}: ${err.message}`);
           }
         });
@@ -38,6 +42,16 @@ ${imageYaml}
   });
 };
 
+const generateThumbnail = (inputPath, file, outputPath)  => {
+  thumb({
+    source: `${inputPath}${file}`,
+    destination: outputPath,
+    prefix: 'thumb_',
+    suffix: ''
+  }, function(err, stdout, stderr) {
+    console.log('All done!');
+  })
+}
 
 const processFiles = folder => {
   const inputPath = `static/${folder}/`;
@@ -47,16 +61,17 @@ const processFiles = folder => {
   for(var i = 0; i < candidates.length; i++) {
     let candidate = candidates[i];
 
-    if(candidate.endsWith('.jpg') || candidate.endsWith('.jpeg') || candidate.endsWith(".png")) {
+    if(candidate.startsWith('thumb_') == false && (candidate.endsWith('.jpg') || candidate.endsWith('.jpeg') || candidate.endsWith(".png"))) {
       files.push(candidate);
+      console.log(candidate)
     }
   }
 
   files.forEach(file => {
-    let mdPath = `${outputPath}/${file}.md`;
-    let thumbnail = `thubmnail-${file}`;
-    generateMarkdown(inputPath, file, mdPath)
-    
+    let mdOutputFile = `${outputPath}${file}.md`;
+    let thumbPath = 'static/life/';
+    generateMarkdown(inputPath, file, mdOutputFile);
+    generateThumbnail(inputPath, file, thumbPath);
 
   });
 }
