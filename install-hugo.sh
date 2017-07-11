ee92f6ab8893b53edfc430b8698958bcfa707ca1
Paul Kinlan <paul.kinlan@gmail.com>
2017-07-11 14:42:04 +0100

    fixing hugo deploy

--- install-hugo.sh
+++ install-hugo.sh
@@ -7,5 +7,5 @@ set -e
 if [ ! -e .hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
   wget --no-check-certificate https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz -P .hugo/
   tar xvzf .hugo/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz -C .hugo/
-  cp .hugo/hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 ./hugo
+  # cp .hugo/hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 ./hugo
 fi
