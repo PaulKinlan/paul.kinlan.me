99565f783dcf93a0fc8b746b87bcaee0fc18104f
Paul Kinlan <paul.kinlan@gmail.com>
2017-07-11 14:23:17 +0100

    Added some life.

--- install-hugo.sh
+++ install-hugo.sh
@@ -1,11 +1,11 @@
-HUGO_VERSION=0.19
+HUGO_VERSION=0.24.1
 
 set -x
 set -e
 
 # Install Hugo if not already cached or upgrade an old version.
 if [ ! -e .hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
-  wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz -P .hugo/
+  wget --no-check-certificate https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz -P .hugo/
   tar xvzf .hugo/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz -C .hugo/
   cp .hugo/hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 ./hugo
 fi
