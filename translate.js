6766557152681ce6a23b1dd3bc800a7a56a796b7
Paul Kinlan <paul.kinlan@gmail.com>
2018-08-30 19:18:39 +0000

    Adding in localisation support

---
+++ translate.js
@@ -0,0 +1,80 @@
+// Imports the Google Cloud client library
+const Translate = require('@google-cloud/translate');
+const program = require('commander');
+const fs = require('fs');
+const path = require('path');
+
+program
+  .version('0.1.0')
+  .option('-s, --source [path]', 'Add in the source file.')
+  .option('-t, --target [lang]', 'Add target language.')
+  .parse(process.argv);
+
+// Creates a client
+const translate = new Translate({
+  projectId: 'html5rocks-hrd'
+});
+
+const options = {
+  to:  program.target,
+};
+
+async function translateLines(text) {
+  if(text === ' ') return ' ';
+  const output = [];
+  let results = await translate.translate(text, options);
+
+  let translations = results[0];
+  translations = Array.isArray(translations)
+    ? translations
+    : [translations];
+
+  translations.forEach((translation, i) => {
+    output.push(translation)
+  });
+
+  return output.join('\n');
+};
+
+// Translates the text into the target language. "text" can be a string for
+// translating a single piece of text, or an array of strings for translating
+// multiple texts.
+(async function (filePath, target) {
+
+  const text = fs.readFileSync(filePath, 'utf8');
+
+  const lines = text.split('\n');
+  let translateBlock = [];
+  const output = [];
+
+  let inHeader = false;
+  let inCode = false;
+  let inQuote = false;
+  for (const line of lines) {
+    // Don't translate preampble
+    if (line.startsWith('---') && inHeader) { inHeader = false; output.push(line); continue; }
+    if (line.startsWith('---')) { inHeader = true; output.push(line); continue; }
+    if (inHeader) { output.push(line); continue; }
+
+    // Don't translate code
+    if (line.startsWith('```') && inCode) { inCode = false; output.push(line); continue; }
+    if (line.startsWith('```')) { inCode = true; output.push(await translateLines(translateBlock.join(' '))); translateBlock = []; output.push(line); continue; }
+    if (inCode) { output.push(line); continue; }
+
+    // Dont translate quotes
+    if (inQuote && line.startsWith('>') === false) { inQuote = false; }
+    if (line.startsWith('>')) { inQuote = true; output.push(await translateLines(translateBlock.join(' '))); translateBlock = []; output.push(line); }
+    if (inQuote) { output.push(line); continue; }
+
+    if (line.charAt(0) === '\n' || line.length === 0) { output.push(await translateLines(translateBlock.join(' '))); output.push(line); translateBlock = []; continue;} 
+
+    translateBlock.push(line);
+  }
+
+  if(translateBlock.length > 0) output.push(await translateLines(translateBlock.join(' ')))
+
+  const result = output.join('\n');
+  const newFileName = path.parse(filePath);
+  fs.writeFileSync(`content/${target}/${newFileName.name}.${target}.${newFileName.ext}`, result);
+
+})(program.source, program.target);
