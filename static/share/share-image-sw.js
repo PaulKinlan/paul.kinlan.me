cbd240b5a3c8ee085203b95f858bec35502abf4d
Paul Kinlan <paul.kinlan@gmail.com>
2019-01-01 15:39:07 +0000

    Adding SW messaging

--- static/share/share-image-sw.js
+++ static/share/share-image-sw.js
@@ -1,11 +1,15 @@
-onfetch = () => {
+onfetch = async () => {
   const url = new URL(event.request.url);
+  const clientId = event.clientId;
+  const hostClient = await self.clients.get(clientId);
 
   // Don't care about other-origin URLs
   if (url.origin !== location.origin) return;
 
   if (url.pathname === '/share/submit-image' && url.searchParams.has('share-target')) {
-    serveShareTarget(event);
+    // Find the window that is open
+    hostClient.postMessage({})
+
     return;
   }
 };
