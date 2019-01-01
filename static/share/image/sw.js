09da92fa5fb57723849d674842201c0da2f443be
Paul Kinlan <paul.kinlan@gmail.com>
2019-01-01 16:08:51 +0000

    Serve the Share even fo rPOST

--- static/share/image/sw.js
+++ static/share/image/sw.js
@@ -1,14 +1,30 @@
 onfetch = async () => {
   const url = new URL(event.request.url);
-  const clientId = event.clientId;
-  const hostClient = await self.clients.get(clientId);
+
+  if (event.request.method !== 'POST') {
+    event.respondWith(fetch(event.request));
+    return;
+  }
 
   // Don't care about other-origin URLs
   if (url.origin !== location.origin) return;
 
   if (url.pathname === '/share/image/' && url.searchParams.has('share-target')) {
-    // Find the window that is open
-    hostClient.postMessage({})
+    const dataPromise = event.request.formData();
+    // Serve the page from the cache:
+
+    event.respondWith(async function () {
+      return fetch('/share/image/');
+    }());
+
+    event.waitUntil(async function () {
+      const client = await self.clients.get(event.resultingClientId);
+      const data = await dataPromise;
+      const file = data.get('file');
+
+      console.log(file, data)
+      client.postMessage({ file, action: 'load-image' });
+    }());
 
     return;
   }
