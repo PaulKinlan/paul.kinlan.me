069ec466a410c31033b13600b0993b4f60156a6d
Paul Kinlan <paul.kinlan@gmail.com>
2019-04-23 11:44:17 +0100

    Fixing up some small head script issues

--- assets/javascripts/head.js
+++ assets/javascripts/head.js
@@ -18,29 +18,6 @@ navigator.serviceWorker.register('/sw.js')
 .then(function(registration) { },
         function(e) { console.log("Service Worker Failure", e); });
 
-document.addEventListener('click', function(e) {
-  var target = e.target;
-  if (target.nodeName === 'DIV' && target.classList.contains('share')) {
-      e.preventDefault();
-
-      var shareUrl = target.getAttribute('url') || '';
-      var shareTitle = target.getAttribute('title') || '';
-      if (navigator.share) {
-      navigator.share({
-          url: shareUrl,
-          title: shareTitle,
-          text: shareTitle
-      }).then(function() { ga('send', 'event', 'share', 'success'); },
-              function() { error => ga('send', 'event', 'share', 'error', error); } );
-      } else {
-      var windowOptions = 'scrollbars=yes,resizable=yes,toolbar=no,location=yes,width=520,height=420';
-      var twitterUrl = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(shareTitle) + '&url=' + encodeURIComponent(shareUrl) + '&via=Paul_Kinlan';
-
-      window.open(twitterUrl, 'intent', windowOptions);
-      }
-  }
-});
-
 window.addEventListener("load", function() {
   var iframes = document.getElementsByTagName("iframe");
   for(var i = 0; i < iframes.length; i++) {
@@ -49,6 +26,28 @@ window.addEventListener("load", function() {
       var src = ifr.getAttribute("data-src");
       ifr.setAttribute("src", src);
     }
+  }
+
+  var shareButtons = document.querySelectorAll('div.share');
+
+  for(var shareButton of shareButtons) {
+    shareButton.addEventListener("click", function(event) {
+      event.preventDefault();
+      var shareUrl = event.target.getAttribute('url') || '';
+      var shareTitle = event.target.getAttribute('title') || '';
+      if (navigator.share) {
+      navigator.share({
+          url: shareUrl,
+          title: shareTitle,
+          text: shareTitle
+      }).then(function() { ga('send', 'event', 'share', 'success'); },
+              function(error) { ga('send', 'event', 'share', 'error', error); } );
+      } else {
+        var windowOptions = 'scrollbars=yes,resizable=yes,toolbar=no,location=yes,width=520,height=420';
+        var twitterUrl = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(shareTitle) + '&url=' + encodeURIComponent(shareUrl) + '&via=Paul_Kinlan';
+        window.open(twitterUrl, 'intent', windowOptions);
+      }
+    });
   }
 });
 
