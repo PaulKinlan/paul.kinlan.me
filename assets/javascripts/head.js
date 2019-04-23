d53ee8f1538504693ef26a6013c736561b320a4b
Paul Kinlan <paul.kinlan@gmail.com>
2019-04-23 11:30:27 +0100

    Some updates to refactor all the things

--- assets/javascripts/head.js
+++ assets/javascripts/head.js
@@ -1,3 +1,19 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-114468-20', 'auto');
+ga('send', 'pageview');
+
+var disqus_shortname = 'paulkinlan'; // required: replace example with your forum shortname
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+window.addEventListener("load", function() {
+  var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+  dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+});
+
 navigator.serviceWorker.register('/sw.js')
 .then(function(registration) { },
         function(e) { console.log("Service Worker Failure", e); });
