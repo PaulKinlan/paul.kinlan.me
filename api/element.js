82fae1024b69bfc08652397b4c49e3e795846a5a
Paul Kinlan <paul.kinlan@gmail.com>
2020-07-06 21:33:07 +0100

    chrome-aws-lambda

--- api/element.js
+++ api/element.js
@@ -1,19 +1,30 @@
-const { go } = require('puppeteer-go');
+const chrome = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
 
 module.exports = async (req, res) => {
   const { url = 'https://paul.kinlan.me/', selector = 'body' } = req.query
+  let data;
 
   try {
-    const data = await go(url, async (page) => {
-      const elements = await page.$$(selector);
-      if (elements && elements.length > 0) {
-        return await elements[0].screenshot({ 'encoding': 'binary' });
-      }
-      else {
-        throw new Error(`Element not found for selector ${selector} in ${url}`);
-      }
+    const browser = await puppeteer.launch({
+      args: chrome.args,
+      executablePath: await chrome.executablePath,
+      headless: chrome.headless,
     });
 
+    const page = await browser.newPage();
+    await page.goto(url);
+    const elements = await page.$$(selector);
+
+    if (elements && elements.length > 0) {
+      data = await elements[0].screenshot({ 'encoding': 'binary' });
+      await browser.close();
+    }
+    else {
+      await browser.close();
+      throw new Error(`Element not found for selector ${selector} in ${url}`);
+    }
+    
     // Add caching.
     res.statusCode = 200;
     res.setHeader('Content-Type', `image/png`);
