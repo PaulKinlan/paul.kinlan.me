f6ae7ec8db83cfe2d7e93d4301d6b8af046cd2f7
Paul Kinlan <paul.kinlan@gmail.com>
2021-10-19 10:23:53 +0100

    fix esbuild

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 // rollup.config.js
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import { esbuild } from 'rollup-plugin-esbuild';
+import esbuild from 'rollup-plugin-esbuild';
 
 export default {
   input: 'static/share/image/main.mjs',
