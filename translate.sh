8badc5db9ba0071030ca26c97ba668fbf4bcda9e
Paul Kinlan <paul.kinlan@gmail.com>
2021-10-22 15:37:49 +0100

    Remove translations and deps

--- translate.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/bash
-export GOOGLE_APPLICATION_CREDENTIALS=../key.json
-find content/en -iname "2019-12*[^.]??.markdown" |
-while read filename
-do 
-  node translate.js -s $filename -t hi,fr,es,ja,de,vi,ru,id,ta,te,pa,pt  \;
-done
-
-#find src/content/en/updates/2018/01/ -iname "*.md" -exec node translate.js -s '{}' -t hi,fr,es,ja,de,vi,ru,id \;
