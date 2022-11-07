1488f9f62d23e46270db001d7d2e29280438633e
Paul Kinlan <paul.kinlan@gmail.com>
2022-11-07 14:06:24 +0000

    Adding updated discovery and actors

---
+++ api/activitypub/actor.js
@@ -0,0 +1,19 @@
+module.exports = (req, res) => {
+  console.log(req)
+  res.statusCode = 200;
+  res.setHeader("Content-Type", `application/activity+json`);
+  res.end(`{
+    "@context": ["https://www.w3.org/ns/activitystreams",
+                 {"@language": ""en-GB"}],
+    "type": "Person",
+    "id": "https://paul.kinlan.me/paul",
+    "outbox": "https://paul.kinlan.me/outbox",
+    "inbox": "https://paul.kinlan.me/inbox",
+    "preferredUsername": "Paul_Kinlan",
+    "name": "Paul Kinlan - Modern Web Development with Chrome",
+    "summary": "Paul is a Developer Advocate for Chrome and the Open Web at Google and loves to help make web development easier.",
+    "icon": [
+      "https://paul.kinlan.me/images/me.png"
+    ]
+  }`);
+};
