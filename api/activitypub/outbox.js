cc50970482aae1c77b777f8b7a6551a9d60ab3a1
Paul Kinlan <paul.kinlan@gmail.com>
2022-11-07 14:34:53 +0000

    API reads from the staticly generated outbox

--- api/activitypub/outbox.js
+++ api/activitypub/outbox.js
@@ -1,6 +1,15 @@
+const path = require('path');
+const process = require('process');
+const {readFileSync} = require('fs');
+
 module.exports = (req, res) => {
   console.log(req)
+
+  const file = path.join(process.cwd(),'public', 'outbox.ajson');
+  const stringified = readFileSync(file, 'utf8');
+
   res.statusCode = 200;
   res.setHeader("Content-Type", `application/activity+json`);
-  res.end('ok');
+
+  return res.end(stringified);
 };
