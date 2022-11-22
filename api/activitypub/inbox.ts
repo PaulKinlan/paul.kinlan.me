b14b3bd3dacc496cdd476e758d3375dd62031ecc
Paul Kinlan <paul.kinlan@gmail.com>
2022-11-22 21:23:30 +0000

    Updating inbox to store request.

--- api/activitypub/inbox.ts
+++ api/activitypub/inbox.ts
@@ -1,5 +1,19 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { AP } from 'activitypub-core-types';
 import type { Readable } from 'node:stream';
+import * as admin from 'firebase-admin';
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    })
+  });
+}
+
+const db = admin.firestore();
 
 export const config = {
   api: {
@@ -15,18 +29,37 @@ async function buffer(readable: Readable) {
   return Buffer.concat(chunks);
 }
 
-export default async function(req: VercelRequest, res: VercelResponse) {
+export default async function (req: VercelRequest, res: VercelResponse) {
   const { body, query, method } = req;
-  
+
+  res.statusCode = 200;
+  res.setHeader("Content-Type", `application/activity+json`);
+
   console.log(method)
   console.log(body, query)
 
   const buf = await buffer(req);
   const rawBody = buf.toString('utf8');
 
-  console.log(rawBody);
+  const message = <AP.Activity>JSON.parse(rawBody);
+  //const object = message.
 
-  res.statusCode = 200;
-  res.setHeader("Content-Type", `application/activity+json`);
-  res.end('ok');
+  if (message.type == "Follow") {
+    // We are following.
+    const obj: AP.Follow = {};
+    
+    const collection = db.collection('follows');
+
+    const followDoc = collection.doc();
+    await followDoc.set(obj);
+
+    // Queue an Accept Activity 
+
+    res.end("ok")
+  }
+
+  if (message.type == "Undo") {
+
+  }
+  res.end();
 };
