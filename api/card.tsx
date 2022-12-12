60d8d2f76d898b2cbd96eeab9d52f217ccfaa415
Paul Kinlan <paul.kinlan@gmail.com>
2022-12-12 20:28:36 +0000

    Image tsx

--- api/card.tsx
+++ api/card.tsx
@@ -1,10 +1,11 @@
-import { ImageResponse } from '@vercel/og'
+import { ImageResponse } from '@vercel/og';
+import { VercelRequest } from '@vercel/node';
 
 export const config = {
-  runtime: 'experimental-edge',
+  runtime: 'experimental-edge'
 }
 
-export default function () {
+export default function (req: VercelRequest) {
 
   const { title = '', description = '', imgUrl = 'https://paul.kinlan.me/images/me.png', width = 800, height = 400 } = req.query;
 
