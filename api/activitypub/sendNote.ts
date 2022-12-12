801dfd0fcce5fe57ef7e3883cfff4d2a0196498d
Paul Kinlan <paul.kinlan@gmail.com>
2022-12-12 15:13:36 +0000

    Updating SendNode

--- api/activitypub/sendNote.ts
+++ api/activitypub/sendNote.ts
@@ -3,9 +3,10 @@ import { AP } from 'activitypub-core-types';
 import type { Readable } from 'node:stream';
 import * as admin from 'firebase-admin';
 import { v4 as uuid } from 'uuid';
-import { CoreObject, Entity } from 'activitypub-core-types/lib/activitypub/index';
+import { CoreObject, Entity, OrderedCollection } from 'activitypub-core-types/lib/activitypub/index';
 import { sendSignedRequest } from '../../lib/activitypub/sendSignedRequest';
 import { parseSignature } from '../../lib/activitypub/utils/parseSignature';
+import { fetchActorInformation } from '../../lib/activitypub/utils/fetchActorInformation';
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
@@ -35,50 +36,16 @@ async function buffer(readable: Readable) {
   return Buffer.concat(chunks);
 }
 
-async function fetchActorInformation(actorUrl: string) {
-  try {
-    const response = await fetch(
-      actorUrl,
-      {
-        headers: {
-          "Content-type": 'application/activity+json',
-          "Accept": 'application/activity+json'
-        }
-      }
-    );
-
-    return await response.json();
-  } catch (error) {
-    console.log("Unable to fetch action information", actorUrl)
-  }
-  return null;
-}
-
-function verifySignature(signature, publicKeyJson) {
-  let signatureValid;
-
-  try {
-    // Verify the signature
-    signatureValid = signature.verify(
-      publicKeyJson.publicKeyPem,	// The PEM string from the public key object
-    );
-  } catch (error) {
-    console.log("Signature Verification error", error)
-  }
-
-  return signatureValid;
-}
 
 /*
   Sends the latest not that hasn't yet been sent.
 */
 export default async function (req: VercelRequest, res: VercelResponse) {
   const { body, query, method, url, headers } = req;
-
   // Check the security token so that we know it was triggered from vercel.
 
-  const collection = db.collection('config');
-  const configRef = collection.doc("config");
+  const configCollection = db.collection('config');
+  const configRef = configCollection.doc("config");
   const config = await configRef.get();
 
   if (config.exists == false) {
@@ -88,85 +55,36 @@ export default async function (req: VercelRequest, res: VercelResponse) {
     });
   }
 
-  res.statusCode = 200;
+  const { lastId } = config.data();
 
-  console.log(method)
-  console.log(body, query)
-  console.log(req.headers);
+  // Get my outbox because it contains all my notes.
 
-  // Verify the message some how.
-  const buf = await buffer(req);
-  const rawBody = buf.toString('utf8');
+  const outboxResponse = await fetch('https://paul.kinlan.me/outbox');
+  const outbox = <OrderedCollection>(await outboxResponse.json());
 
-  const message = <AP.Activity>JSON.parse(rawBody);
+  const followersCollection = db.collection('followers');
+  const followersQuerySnapshot = await followersCollection.get();
 
-  console.log(message);
+  for (const followerDoc of followersQuerySnapshot.docs) {
+    const follower = followerDoc.data();
+    try {
+      const actorInformation = await fetchActorInformation(follower.actor);
+      const actorInbox = new URL(actorInformation.inbox);
 
-  const signature = parseSignature(req);
-  const actorInformation = await fetchActorInformation(signature.keyId);
-  const signatureValid = verifySignature(signature, actorInformation.publicKey);
+      for (const iteIdx in (<AP.EntityReference[]>outbox.orderedItems)) {
+        // We have to break somewhereh... do it after the first.
+        const item = (<AP.EntityReference[]>outbox.orderedItems)[iteIdx];
+        // Item will be an entity, i.e, { Create { Note } }
+        const response = await sendSignedRequest(actorInbox, item);
+        console.log("Following result", actorInbox, response.status, response.statusText, await response.text());
 
-  if (signatureValid == null || signatureValid == false) {
-    res.end('invalid signature');
-    return;
-  }
+        break;
+      }
 
-  // We should check the digest.
-  if (message.type == "Follow") {
-    console.log('Follow');
-    // We are following.
-    const followMessage: AP.Follow = <AP.Follow>message;
-    if (followMessage.id == null) return;
-
-    const collection = db.collection('followers');
-
-    const actorID = (<URL>followMessage.actor).toString();
-    const followDocRef = collection.doc(actorID.replace(/\//g, "_"));
-    const followDoc = await followDocRef.get();
-
-    if (followDoc.exists) {
-      console.log("Already Following")
-      return res.end('already following');
+    } catch (ex) {
+      console.log("Error", ex, follower);
     }
-
-    // Create the follow;
-    await followDocRef.set(followMessage);
-
-    const guid = uuid();
-    const domain = 'paul.kinlan.me';
-
-    const acceptRequest: AP.Accept = <AP.Accept>{
-      "@context": "https://www.w3.org/ns/activitystreams",
-      'id': new URL(`https://${domain}/${guid}`),
-      'type': 'Accept',
-      'actor': "https://paul.kinlan.me/paul",
-      'object': followMessage
-    };
-
-    const actorInbox = new URL(actorInformation.inbox);
-
-    const response = await sendSignedRequest(actorInbox, acceptRequest);
-
-    console.log("Following result", response.status, response.statusText, await response.text());
-
-    return res.end("ok")
   }
 
-  if (message.type == "Undo") {
-    // Undo a follow.
-    const undoObject: AP.Undo = <AP.Undo>message;
-    if (undoObject == null || undoObject.id == null) return;
-    if (undoObject.object == null) return;
-    if ("actor" in undoObject.object == false && (<CoreObject>undoObject.object).type != "Follow") return;
-
-    const docId = undoObject.actor.toString().replace(/\//g, "_");
-
-    console.log("DocId to delete", docId)
-
-    const res = await db.collection('followers').doc(docId).delete();
-
-    console.log("Deleted", res)
-  }
-
-  res.end();
+  res.status(200).end("ok");
 };
---
+++ lib/activitypub/utils/fetchActorInformation.ts
@@ -0,0 +1,18 @@
+export async function fetchActorInformation(actorUrl: string) {
+  try {
+    const response = await fetch(
+      actorUrl,
+      {
+        headers: {
+          "Content-type": 'application/activity+json',
+          "Accept": 'application/activity+json'
+        }
+      }
+    );
+
+    return await response.json();
+  } catch (error) {
+    console.log("Unable to fetch action information", actorUrl);
+  }
+  return null;
+}
