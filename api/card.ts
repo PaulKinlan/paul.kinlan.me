a2f1e2b29702949db38a273c1258cbbce0f906b5
Paul Kinlan <paul.kinlan@gmail.com>
2022-12-12 22:21:11 +0000

    Improving Card Parameter handling

--- api/card.ts
+++ api/card.ts
@@ -9,6 +9,9 @@ export default function (req: Request) {
   const url = new URL(req.url);
   const title = url.searchParams.get("title");
   const imgUrl = url.searchParams.get("imgUrl") || "https://paul.kinlan.me/images/me.png";
+  const width = url.searchParams.get("width") || "800"
+  const height = url.searchParams.get("width") || "418";
+
 
   console.log(req);
 
@@ -23,5 +26,5 @@ export default function (req: Request) {
         height: "100%",
       }
     }
-  }, { width: 800, height: 418 }); // 800px by 418px
+  }, { width, height }); // 800px by 418px
 }
