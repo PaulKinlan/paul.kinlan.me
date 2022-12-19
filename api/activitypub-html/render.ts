27e924f1b7d6c5d8bac2f940c65c084a88332d9e
Paul Kinlan <paul.kinlan@gmail.com>
2022-12-19 11:03:55 +0000

    Formatting and Content escaping

--- api/activitypub-html/render.ts
+++ api/activitypub-html/render.ts
@@ -1,6 +1,14 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import * as admin from 'firebase-admin';
 
+const escapeHTML = (str: string): string => str.replace(/[<>'"]/g,
+  tag => ({
+    '<': '&lt;',
+    '>': '&gt;',
+    "'": '&#39;',
+    '"': '&quot;'
+  }[tag]));
+
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 if (!admin.apps.length) {
@@ -43,34 +51,59 @@ export default async function (req: VercelRequest, res: VercelResponse) {
   res.end(`<doctype html>
   <html>
     <head>
-      <title>ActivityPub HTML</title>
+      <title>Interactions from around the fediverse with ${escapeHTML(url)}</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <style>
+      @media (prefers-color-scheme: dark) {
+        html {
+          color: #fefefe;
+          background-color: #212529;
+        }
+      
+        a {
+          color: #1bcba2
+        }
+      
+        a:visited {
+          color: #7ad857
+        }
+      }
+
+      a img.profile {
+        width: 32px;
+        height: 32px;
+        border-radius: 50% 50%;
+      }
+      </style>
     </head>
     <body>  
-      <h1>ActivityPub HTML</h1>
-      <p>URL: ${url}</p>
+      <h1>Iteractions from around the fediverse</h1>
+      <p>URL: ${escapeHTML(url)}</p>
       <h2>Likes</h2>
       <p>Count: ${likesCount}</p>
       <ul>
         ${likesSnapshot.docs.map(doc => {
     const { actor } = doc.data();
     if (typeof actor == "string") {
-      return `<li><a href="${actor}" rel="nofollow">${actor}</a></li>`;
+      return `<li><a href="${escapeHTML(actor)}" rel="nofollow">${escapeHTML(actor)}</a></li>`;
     }
 
-    return `<li><a href="${actor.url}" rel="nofollow"><img src="${actor.icon.url}" alt="${actor.name}"></a></li>`
+    return `<li><a href="${escapeHTML(actor.url)}" rel="nofollow"><img class="profile" src="${escapeHTML(actor.icon.url)}" alt="${escapeHTML(actor.name)}"></a></li>`
   }
   ).join("")}
-        </ul>
+      </ul>
       <h2>Announces</h2>
       <p>Count: ${announcesCount}</p>
       <ul>
         ${announcesSnapshot.docs.map(doc => {
     const { actor } = doc.data();
     if (typeof actor == "string") {
-      return `<li><a href="${actor}" rel="nofollow">${actor}</a></li>`;
+      return `<li><a href="${escapeHTML(actor)}" rel="nofollow">${escapeHTML(actor)}</a></li>`;
     }
 
-    return `<li><a href="${actor.url}" rel="nofollow"><img src="${actor.icon.url}" alt="${actor.name}"></a></li>`
+    return `<li><a href="${escapeHTML(actor.url)}" rel="nofollow"><img class="profile" src="${escapeHTML(actor.icon.url)}" alt="${escapeHTML(actor.name)}"></a></li>`
   }
   ).join("")}
       </ul>
