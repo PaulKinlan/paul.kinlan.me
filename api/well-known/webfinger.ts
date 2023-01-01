e5f3fc5978fbc234359bca69691454606607f90d
Paul Kinlan <paul.kinlan@gmail.com>
2023-01-01 19:35:27 +0000

    In the future we might use the resouce.

--- api/well-known/webfinger.ts
+++ api/well-known/webfinger.ts
@@ -1,6 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function (req: VercelRequest, res: VercelResponse) {
+  const { resource } = req.query;
   res.statusCode = 200;
   res.setHeader("Content-Type", `application/jrd+json`);
   res.end(`{  
