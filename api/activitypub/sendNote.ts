3dd2b3cdfa79516bc7d4be9987d64f6e4501dd4b
Paul Kinlan <paul.kinlan@gmail.com>
2023-01-11 22:04:40 +0000

    nullish checks to stop complaining

--- api/activitypub/sendNote.ts
+++ api/activitypub/sendNote.ts
@@ -12,7 +12,7 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
     })
   });
 }
