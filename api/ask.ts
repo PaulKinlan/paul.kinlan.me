95ae0629bc9e2ea436b2f4e4f20502d595b56255
Paul Kinlan <paul.kinlan@gmail.com>
2023-03-03 22:32:26 +0000

    logging

--- api/ask.ts
+++ api/ask.ts
@@ -18,7 +18,7 @@ export default async function (req: Request) {
   const url = new URL(req.url);
   const question = url.searchParams.get("question") || "What are Web Intents?";
 
-  console.log(req);
+  console.log(proto, host, url);
 
   const response = fetch(`${proto}://${host}/api/polymath.ts?question=${question}`);
 
