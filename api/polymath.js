32d605fccaf5a965bc68f49f67f426da778ef1e5
Paul Kinlan <paul.kinlan@gmail.com>
2023-03-03 23:54:20 +0000

    Trying gpt-turbo

--- api/polymath.js
+++ api/polymath.js
@@ -3,12 +3,15 @@ import { Polymath } from "@polymath-ai/client/main.js";
 export default async function (req, res) {
   res.contentType = "application/json";
   const { question = "What are web intents?" } = req.query;
-  
+
   let p = new Polymath({
     apiKey: process.env.OPENAI_API_KEY,
     pinecone: {
       namespace: process.env.PINECONE_NAMESPACE,
     },
+    completionOptions: {
+      model: "gpt-3.5-turbo"
+    }
   });
 
   let response = await p.completion(question);
