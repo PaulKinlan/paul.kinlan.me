e57d9f465ab4c78e31306d3761b1130b97d17f6f
Paul Kinlan <paul.kinlan@gmail.com>
2023-03-06 18:52:10 +0000

    Updating to new polymath lib

--- api/polymath/ask.ts
+++ api/polymath/ask.ts
@@ -1,6 +1,13 @@
-import { Polymath } from "@polymath-ai/client/main.js";
+import { Polymath } from "@polymath-ai/client";
 import { Form } from "multiparty";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+function decodeEmbedding(data: string) {
+  const buffer = Buffer.from(data, "base64");
+  return Array.from(
+    new Float32Array(new Uint8Array(buffer).buffer)
+  );
+}
 
 export default async function (req: VercelRequest, res: VercelResponse) {
   res.setHeader('content-type', 'application/json');
@@ -29,25 +36,30 @@ export default async function (req: VercelRequest, res: VercelResponse) {
       form.parse(req, (err: any, fields: any) => {
         const entries = Object.entries(fields);
         askQuery = fields["query"] || askQuery;
-        const otherOptions = {};
+        const otherOptions: { [key: string]: any } = {};
         for (const [key, value] of entries) {
           if (key !== "query") otherOptions[key] = value;
         }
 
-        return p.ask(askQuery, otherOptions).then(polymathResponse => {
+        if ("query_embedding" in otherOptions) {
+          otherOptions["query_embedding"] = decodeEmbedding(otherOptions["query_embedding"][0]);
+        }
+
+        return p.ask(askQuery, otherOptions).then((polymathResponse: any) => {
           const bits = polymathResponse.bits();
-    
-          return res.send({ 
-            version: 1, 
+
+          return res.send({
+            version: 1,
             "embedding_model": otherOptions.query_embedding_model[0],
             "omit": "embedding",
             "//sort": "similarity",
             "details": {
               "counts": {
-                  "bits": bits.length
+                "bits": bits.length
               }
-          },
-            "bits": bits })
+            },
+            "bits": bits
+          })
         }
         )
       });
