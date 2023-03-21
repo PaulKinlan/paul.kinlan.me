50b6ba800026b453f05285d4221cef05bdb570ea
Paul Kinlan <paul.kinlan@gmail.com>
2023-03-21 21:03:40 +0000

    Adding GA.

--- api/ask.ts
+++ api/ask.ts
@@ -28,6 +28,17 @@ export default async function (req: Request) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="/css/ask.css">
+        <script>
+        (function (i, s, o, g, r, a, m) {
+          i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+          }, i[r].l = 1 * new Date(); a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+        
+        ga('create', 'UA-114468-20', 'auto');
+        ga('send', 'pageview');
+        </script>
       </head>
       <body>
       <header>
