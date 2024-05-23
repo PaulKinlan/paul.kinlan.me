a86bd22d01401297485da2ebeb3c95ae53447679
Paul Kinlan <paul.kinlan@gmail.com>
2024-05-23 18:12:12 +0000

    aDding fedcm

---
+++ api/well-known/web-identity.ts
@@ -0,0 +1,14 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default function (req: VercelRequest, res: VercelResponse) {
+  const { resource } = req.query;
+  res.statusCode = 200;
+  res.setHeader(
+    "Content-Type",
+    `application/json;`
+  );
+  res.json({
+  "provider_urls":
+     ["https://sso.kinlan.me/test/fedcm.json"]
+});
+}
