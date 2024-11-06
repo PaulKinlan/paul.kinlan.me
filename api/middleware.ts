1b2983a538b3f079681e82dc4cf291eda55ebd9d
Paul Kinlan <paul.kinlan@gmail.com>
2024-11-06 14:03:38 +0000

    kv

---
+++ api/middleware.ts
@@ -0,0 +1,23 @@
+import { next } from "@vercel/edge";
+import { kv } from "@vercel/kv";
+
+export const config = {
+  matcher: "/",
+};
+
+export default async function middleware(request: Request) {
+
+  const { headers } = request;
+  const { userAgent } = headers.values['User-Agent'];
+
+  try {
+    // 30 days.
+    await kv.set(userAgent, 0, { ex: 30 * 86400, nx: true });
+    await kv.incr(userAgent);
+    console.log(`Logging User Agent ${userAgent}`);
+  } catch (error) {
+    // Handle errors
+    console.log(error.message);
+  }
+  return next();
+}
