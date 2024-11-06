7c7fcdbf28eeea06a7de237ab178102812902462
Paul Kinlan <paul.kinlan@gmail.com>
2024-11-06 14:22:18 +0000

    userAgent

--- middleware.ts
+++ middleware.ts
@@ -8,7 +8,7 @@ export const config = {
 export default async function middleware(request: Request) {
 
   const { headers } = request;
-  const { userAgent } = headers.values['User-Agent'];
+  const userAgent = headers.values['userAgent'];
 
   try {
     // 30 days.
