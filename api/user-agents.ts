32c7b816587cfc40aa4673853bfd9b1bbebd5fac
Paul Kinlan <paul.kinlan@gmail.com>
2024-11-06 15:24:03 +0000

    res

--- api/user-agents.ts
+++ api/user-agents.ts
@@ -1,8 +1,10 @@
 import { kv } from "@vercel/kv";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default async function userAgents(request: Request, response: Response) {
+
+export default async function userAgents(request: VercelRequest, response: VercelResponse) {
   let userAgents = [];
-  let cursor: string| number = "0";
+  let cursor: string | number = "0";
   let pattern = "*";
 
   try {
@@ -10,22 +12,24 @@ export default async function userAgents(request: Request, response: Response) {
     //userAgents = await kv.keys("*")
 
     do {
-     const [newCursor, matchingKeys] = await kv.scan(
-        cursor,
-        { match: pattern }
+      const [newCursor, matchingKeys] = await kv.scan(cursor, {
+        match: pattern,
+      });
+      console.log(
+        `Scanning, ${matchingKeys.length}, Old Cursor ${cursor}, New Cursor ${newCursor} `
       );
-      console.log(`Scanning, ${matchingKeys.length}, Old Cursor ${cursor}, New Cursor ${newCursor} `);
 
       cursor = newCursor;
       userAgents.push(matchingKeys.flat());
-    } while(cursor != "0")
+    } while (cursor != "0");
 
-      console.log(`Scanning finished, ${userAgents.length}`);
-
+    console.log(`Scanning finished, ${userAgents.length}`);
   } catch (error) {
     // Handle errors
     console.log(error.message);
   }
 
-  return new Response(JSON.stringify(userAgents), { headers: { "content-type": "application/json" } });
+  response.statusCode = 200;
+  response.setHeader("Content-Type", `application/json`);
+  response.json(userAgents);
 }
